a348b77e661e0a41742b8960673df38b3b064b41
prashant <prashant@nineleaps.com>
2019-02-26 19:05:54 +0530

    ci: 🎡 Added connection with mlab mongoDB database

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const Hapi = require('hapi');
+const mongoose = require('mongoose')
+//mongoose.connect('mongodb://<prashantpersie>:<luckkadon007>@ds015574.mlab.com:15574/hapigraphql',{ useNewUrlParser: true })
 const server = Hapi.Server({
   port: 3000,
   host: 'localhost'
 });
+var mongodbUri ='mongodb://@ds015574.mlab.com:15574/hapigraphql';
+mongoose.connect(mongodbUri, {
+  useNewUrlParser: true,
+  auth: {
+    user: 'prashantpersie',
+    password: 'luckkadon007'
+  }
+})
+var conn = mongoose.connection;    
+conn.on('error', console.error.bind(console, 'connection error:'));  
+ 
+conn.once('open', () =>{
+ console.log('connected to database')                       
+});
 
 const init = async () => {
+  server.route({
+    method : 'GET',
+    path : '/',
+    handler : (req,res)=>{
+      return 'Welcome to my Hapi Project Demo'
+    }
+  })
   await server.start();
   console.log(`Server started on port ${server.info.uri}`);
 };
