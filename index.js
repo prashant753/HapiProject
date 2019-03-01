8320a63f27fe63964cdfa675197d4aaed9531f8e
prashant <prashant@nineleaps.com>
2019-03-01 18:40:00 +0530

    feat: 🎸 Added swagger documentation

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const Hapi = require('hapi');
 const mongoose = require('mongoose');
 const Painting = require('./models/painting');
 const schema = require('./graphql/schema')
-const {graphqlHapi,graphiqlHapi} = require('apollo-server-hapi')
+const {graphqlHapi,graphiqlHapi} = require('apollo-server-hapi');
+/* swagger section */
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./package');
 //mongoose.connect('mongodb://<prashantpersie>:<luckkadon007>@ds015574.mlab.com:15574/hapigraphql',{ useNewUrlParser: true })
 const server = Hapi.Server({
   port: 8097,
@@ -26,19 +31,19 @@ conn.once('open', () =>{
 const init = async () => {
 
 
-	// await server.register([
-	// 	Inert,
-	// 	Vision,
-	// 	{
-	// 		plugin: HapiSwagger,
-	// 		options: {
-	// 			info: {
-	// 				title: 'Paintings API Documentation',
-	// 				version: Pack.version
-	// 			}
-	// 		}
-	// 	}
-	// ]);
+	await server.register([
+		Inert,
+		Vision,
+		{
+			plugin: HapiSwagger,
+			options: {
+				info: {
+					title: 'Paintings API Documentation',
+					version: Pack.version
+				}
+			}
+		}
+	]);
 
 	await server.register({
 		plugin: graphiqlHapi,
@@ -53,48 +58,51 @@ const init = async () => {
 		}
 	});
 
-	// await server.register({
-	// 	plugin: graphqlHapi,
-	// 	options: {
-	// 		path: '/graphql',
-	// 		graphqlOptions: {
-	// 			schema
-	// 		},
-	// 		route: {
-	// 			cors: true
-	// 		}
-	// 	}
-	// });
+	await server.register({
+		plugin: graphqlHapi,
+		options: {
+			path: '/graphql',
+			graphqlOptions: {
+				schema
+			},
+			route: {
+				cors: true
+			}
+		}
+	});
 
-  server.route([
-    {
-      method : 'GET',
-      path : '/',
-      handler : (req,res)=>{
-        return 'Welcome to my Hapi Project Demo'
-      }
-    },
-    {
-      method : 'POST',
-      path : '/paintings',
-      handler : (req,res)=>{
-        const {name,url,technique} = req.payload;
-        const painting = new Painting({
-          name,
-          url,
-          technique
-        })
-        return painting.save();
-      }
-    },
-    {
-      method : 'GET',
-      path : '/paintings',
-      handler : (req,res)=>{
-        return Painting.find();
-    }
-  }
-  ])
+
+	server.route([
+		{
+			method: 'GET',
+			path: '/api/v1/paintings',
+			config: {
+				description: 'Get all the paintings',
+				tags: ['api', 'v1', 'painting']
+			},
+			handler: (req, reply) => {
+				return Painting.find();
+			}
+		},
+		{
+			method: 'POST',
+			path: '/api/v1/paintings',
+			config: {
+				description: 'Get a specific painting by ID.',
+				tags: ['api', 'v1', 'painting']
+			},
+			handler: (req, reply) => {
+				const { name, url, technique } = req.payload;
+				const painting = new Painting({
+					name,
+					url,
+					technique
+				});
+
+				return painting.save();
+			}
+		}
+	]);
   await server.start();
   console.log(`Server started on port ${server.info.uri}`);
 };
